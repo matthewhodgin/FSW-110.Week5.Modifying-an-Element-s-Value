4dd523ce78f88e77c5ea0ab17d9de313c9f3b09f
matthewhodgin <95104960+matthewhodgin@users.noreply.github.com>
2022-03-04 14:04:42 -0700

    Add files via upload

--- Week5-Modifying Element's Value.js
+++ Week5-Modifying Element's Value.js
@@ -1,12 +1,16 @@
-const inputElement = document.getElementById('text-input')
-inputElement.value = ''
-
-
 var x1=document.getElementById('text1').value;
 var x2=document.getElementById('text2').value;
 var x3=document.getElementById('text3').value;
  
-alert("First Name  "+x2+"\n Last Name "+x3+"\n Email ");
+document.getElementById('button').addEventListener("click",Submit);
+function Submit(){
+
+    alert("First Name  "+document.getElementById('text1').value+
+    "\nLast Name "+document.getElementById('text2').value+
+    "\nEmail "+document.getElementById('text3').value);
+}
 
 
-
+document.getElementById('text1').style.marginLeft= "20px";
+document.getElementById('text2').style.marginLeft= "20px";
+document.getElementById('text3').style.marginLeft= "20px";
